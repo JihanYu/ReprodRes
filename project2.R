6039fbb0819e793fab74961ad168604efd0053a6
JihanYu <styjh80@gmail.com>
2020-03-02 18:00:07 +0900

    modify project2.R

--- project2.R
+++ project2.R
@@ -7,7 +7,7 @@ setwd(workingpath)
 # results : main results
 # figure : 1-3
 # start from the raw data file
-# which types of events are most harmful to population health
+# which types of events(EVTYPE) are most harmful to population health
 # which types of events have the greatest economic consequences
 # reproducible
 # figure(s) - descriptive captions
@@ -16,3 +16,13 @@ setwd(workingpath)
 download.file ("https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2", "stormdata.csv.bz2")
 stormdata <- read.csv("stormdata.csv.bz2", header=TRUE)
 
+event <- c("ASTRONOMICAL LOW TIDE", "AVALANCHE", "BLIZZARD", "COASTAL FLOOD", "COLD/WIND CHILL",
+           "DEBRIS FLOW", "DENSE FOG", "DENSE SMOKE", "DROUGHT", "DUST DEVIL",
+		   "DUST STORM", "EXCESSIVE HEAT", "EXTREME COLD/WIND CHILL", "FLASH FLOOD", "FLOOD",
+		   "FROST/FREEZE", "FUNNEL CLOUD", "FREEZING FOG", "HAIL", "HEAT",
+		   "HEAVY RAIN", "HEAVY SNOW", "HIGH SURF", "HIGH WIND", "HURRICANE",
+		   "ICE STORM", "LAKE-EFFECT SNOW", "LAKESHORE FLOOD", "LIGHTNING", "MARINE HAIL",
+		   "MARINE HIGH WIND", "MARINE STRONG WIND", "MARINE THUNDERSTORM WIND", "RIP CURRENT", "SEICHE",
+		   "SLEET", "STORM SURGE/TIDE", "STRONG WIND", "THUNDERSTORM WIND", "TORNADO",
+		   "TROPICAL DEPRESSION", "TROPICAL STORM", "TSUNAMI", "VOLCANIC ASH", "WATERSPOUT",
+		   "WILDFIRE", "WINTER STORM", "WINTER WEATHER")
